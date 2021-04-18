5fed21c4bd505f04d3509b5ce1eedf3d5eba2787
ymdkit <yamada-k2021@s.okayama-u.ac.jp>
2021-04-18 09:15:36 +0900

    Add prettier setting

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,25 @@
-import Discord from "discord.js";
-import shuffle from "shuffle-array";
+import Discord from 'discord.js'
+import shuffle from 'shuffle-array'
 
-const client = new Discord.Client();
+const client = new Discord.Client()
 
-client.on("ready", () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`)
+})
 
-client.on("message", (msg) => {
-  if (msg.content === "shuffle") {
-    const channel = msg.member.voice.channel;
-    const members = channel.members.array();
+client.on('message', (msg) => {
+  if (msg.content === 'shuffle') {
+    const channel = msg.member.voice.channel
+    const members = channel.members.array()
     const voiceChannels = client.channels.cache
       .array()
-      .filter((it) => it.name.includes("スタディ"));
+      .filter((it) => it.name.includes('スタディ'))
 
-    shuffle(members);
+    shuffle(members)
     members.forEach((member, i) => {
-      member.voice.setChannel(voiceChannels[i % voiceChannels.length]);
-    });
+      member.voice.setChannel(voiceChannels[i % voiceChannels.length])
+    })
   }
-});
+})
 
-client.login(process.env.AUTH_TOKEN);
+client.login(process.env.AUTH_TOKEN)
